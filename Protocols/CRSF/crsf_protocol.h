8d1df09d9d2c7c49c73bca32f59321b03452d687
huyanhh195 <huyanhh195@gmail.com>
2026-03-21 10:01:32 +0700

    Add new protocols

---
+++ Protocols/CRSF/crsf_protocol.h
@@ -0,0 +1,40 @@
+/*
+ * REF: https://github.com/crsf-wg/crsf/wiki/Message-Format
+ * Format: [address] [len] [type] [payload] [crc8]
+ * */
+
+// frame type
+// địa chỉ
+// max length
+
+#ifndef _CRSF_PROTOCOL_H_
+#define _CRSF_PROTOCOL_H_
+
+#define CRSF_MAX_CHANNEL 16
+#define CRSF_BITS_PER_CHANNEL 11
+#define CRSF_MAX_PAYLOAD_SIZE 60
+#define CRSF_MAX_PACKET_SIZE (CRSF_MAX_PAYLOAD_SIZE + 4) // PayloadLength+4 (sync, len, type, crc)
+
+#define CRSF_SYNC_BYTE (0xC8)                    // Address: Flight Controller
+#define CRSF_FRAMETYPE_RC_CHANNELS_PACKED (0x16) // Frame types
+
+typedef struct
+{
+    uint8_t sync_byte;
+    uint8_t len;
+    uint8_t type;
+    uint8_t payload[CRSF_MAX_PAYLOAD_SIZE];
+    uint8_t crc;
+} crsf_frame_t;
+
+// TODO: Add more channels
+typedef struct
+{
+    uint8_t ch0;
+    uint8_t ch1;
+    uint8_t ch2;
+    uint8_t ch3;
+    uint8_t ch4;
+} crsf_channels_t;
+
+#endif //_CRSF_PROTOCOL_H_
