ca476d71a22d4bdb46025a6ec2d4750ad3d2b056
LongDo1902 <dobaolong2002@gmail.com>
2026-05-27 14:33:22 +0700

    FreeRTOS for IMU task backup

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -27,6 +27,7 @@
 /* USER CODE BEGIN Includes */
 #include "imu/icm42688_application.h"
 #include "logging.h"
+#include "spi.h"
 
 /* USER CODE END Includes */
 
@@ -47,29 +48,31 @@
 
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN Variables */
+ICM42688_Handle_t                 icm42688_handle     = {0};
+ICM42688_Offset_Raw_t             icm42688_offset_raw = {0};
+ICM42688_Temp_Accel_Gyro_Scaled_t icm42688_scaled     = {0};
+ICM42688_Est_Angle_complement_t   icm42688_est_angle  = {0};
+
 osThreadId_t         IMUTask;
 const osThreadAttr_t IMUTaskAttributes = {
     .name       = "IMUTask",
     .stack_size = 128 * 4,
-    .priority   = osPriorityRealtime7,
+    .priority   = osPriorityHigh7,
 };
 
-
+osThreadId_t         LoggingTask;
+const osThreadAttr_t LoggingTaskAttributes = {
+    .name       = "LoggingTask",
+    .stack_size = 128 * 1,
+    .priority   = osPriorityNormal,
+};
 /* USER CODE END Variables */
-/* Definitions for defaultTestTask */
-osThreadId_t         defaultTestTaskHandle;
-const osThreadAttr_t defaultTestTask_attributes = {
-    .name       = "defaultTestTask",
-    .stack_size = 128 * 4,
-    .priority   = (osPriority_t)osPriorityNormal,
-};
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
-
+void StartIMUTask(void *argument);
+void StartLoggingTask(void *argument);
 /* USER CODE END FunctionPrototypes */
-
-void StartDefaultTestTask(void *argument);
 
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
@@ -102,11 +105,10 @@ MX_FREERTOS_Init(void)
     /* USER CODE END RTOS_QUEUES */
 
     /* Create the thread(s) */
-    /* creation of defaultTestTask */
-    defaultTestTaskHandle = osThreadNew(StartDefaultTestTask, NULL, &defaultTestTask_attributes);
-
     /* USER CODE BEGIN RTOS_THREADS */
     /* add threads, ... */
+    IMUTask     = osThreadNew(StartIMUTask, NULL, &IMUTaskAttributes);
+    LoggingTask = osThreadNew(StartLoggingTask, NULL, &LoggingTaskAttributes);
 
     /* USER CODE END RTOS_THREADS */
 
@@ -115,19 +117,38 @@ MX_FREERTOS_Init(void)
     /* USER CODE END RTOS_EVENTS */
 }
 
-/* USER CODE BEGIN Header_StartDefaultTestTask */
-
-/* USER CODE END Header_StartDefaultTestTask */
-void
-StartDefaultTestTask(void *argument)
-{
-    /* USER CODE BEGIN StartDefaultTestTask */
-    /* Infinite loop */
-
-    /* USER CODE END StartDefaultTestTask */
-}
-
 /* Private application code --------------------------------------------------*/
 /* USER CODE BEGIN Application */
+void
+StartIMUTask(void *argument)
+{
+    icm42688_handle.spi_config.hspi    = &hspi1;
+    icm42688_handle.spi_config.cs_port = GPIOA;
+    icm42688_handle.spi_config.cs_pin  = GPIO_PIN_4;
 
+    if (!ICM42688_Init(&icm42688_handle))
+        return;
+    osDelay(5);
+
+    if (!ICM42688_Get_Calibrate_Raw(&icm42688_handle, &icm42688_offset_raw, 200))
+        return;
+    osDelay(5);
+
+    for (;;) {
+        if (!ICM42688_Get_Temp_Accel_Gyro_Scaled(&icm42688_handle, &icm42688_offset_raw,
+                                                 &icm42688_scaled))
+            return;
+
+        if (!ICM42688_Get_Est_Angle_Complement(&icm42688_handle, IMU_ORIENT_NEGY_NEGX_NEGZ,
+                                               &icm42688_scaled, &icm42688_est_angle, ))
+            return;
+    }
+}
+
+void
+StartLoggingTask(void *argument)
+{
+    for (;;) {
+    }
+}
 /* USER CODE END Application */
