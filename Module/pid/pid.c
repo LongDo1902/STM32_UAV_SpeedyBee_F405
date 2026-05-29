cfa94535d18623acb9e04427df746b8b67bd3ecf
huypqa <huyanhh195@gmail.com>
2026-05-29 12:20:57 +0700

    cascade pid

---
+++ Module/pid/pid.c
@@ -0,0 +1,78 @@
+#include "pid.h"
+
+uint16_t
+pid_compute(pid_controller_t *pid, const float setpoint_angle, const float angle, const float rate)
+{
+    /* OUTER CONTROLLER FOR ANGLE */
+    pid->outer_loop.setpoint = setpoint_angle;
+    pid->outer_loop.input    = angle;
+
+    pid->outer_loop.error = pid->outer_loop.setpoint - pid->outer_loop.input;
+    pid->outer_loop.error_sum += pid->outer_loop.error * pid->outer_loop.sample_time;
+    pid->outer_loop.error_deriv = -rate;
+
+    // anti wind-up for error outer controller
+    clamp(&pid->outer_loop.error_sum, &pid->outer_loop.error_sum_max,
+          &pid->outer_loop.error_sum_min);
+
+    // TODO: filer D gains
+
+    pid->outer_loop.output = pid->outer_loop.gains.kp * pid->outer_loop.error +
+                             pid->outer_loop.gains.ki * pid->outer_loop.error_sum +
+                             pid->outer_loop.gains.kd * pid->outer_loop.error_deriv;
+
+    /* INNER CONTROLLER FOR RATE */
+    pid->inner_loop.setpoint = pid->outer_loop.output;
+    pid->inner_loop.input    = rate;
+
+    pid->inner_loop.error = pid->inner_loop.setpoint - pid->inner_loop.input;
+    pid->inner_loop.error_sum += pid->inner_loop.error * pid->inner_loop.sample_time;
+
+    pid->inner_loop.error_deriv = -(pid->inner_loop.input - pid->inner_loop.input_prev);
+    pid->inner_loop.error_deriv = pid->inner_loop.error_deriv / pid->inner_loop.sample_time;
+    pid->inner_loop.input_prev  = pid->inner_loop.input;
+
+    // anti wind-up for error inner controller
+    clamp(&pid->inner_loop.error_sum, &pid->inner_loop.error_sum_max,
+          &pid->inner_loop.error_sum_min);
+
+    // TODO: filer D gains
+
+    pid->inner_loop.output = pid->inner_loop.gains.kp * pid->inner_loop.error +
+                             pid->inner_loop.gains.ki * pid->inner_loop.error_sum +
+                             pid->inner_loop.gains.kd * pid->inner_loop.error_deriv;
+
+    return pid->inner_loop.output;
+}
+
+uint8_t
+pid_get_gains(pid_controller_t *pid, pid_loop_t loop, pid_gains_t *gains)
+{
+    if (!pid || !gains) {
+        return false;
+    }
+
+    switch (loop) {
+        case PID_LOOP_OUTER:
+            *gains = pid->outer_loop.gains;
+            break;
+        case PID_LOOP_INNER:
+            *gains = pid->inner_loop.gains;
+            break;
+        default:
+            return false;
+    }
+
+    return true;
+}
+
+void
+clamp(float *value, const float *max, const float *min)
+{
+    if (*value > *max) {
+        *value = *max;
+    }
+    else if (*value < *min) {
+        *value = *min;
+    }
+}
---
+++ Module/pid/pid.h
@@ -0,0 +1,55 @@
+#ifndef _PID_H_
+#define _PID_H_
+
+// PID Gains
+typedef struct
+{
+    float kp;
+    float ki;
+    float kd;
+} pid_gains_t;
+
+// Parameter of PID Controller
+typedef struct
+{
+    pid_gains_t gains;
+
+    float setpoint;
+    float input;
+    float input_prev;
+    float output;
+
+    float error;
+    float error_sum;
+    float error_deriv;
+
+    uint32_t sample_time;
+
+    float error_sum_max;
+    float error_sum_min;
+
+} pid_def_t;
+
+// Cascaded PID controller
+typedef struct
+{
+    pid_def_t outer_loop; // angle
+    pid_def_t inner_loop; // rate
+} pid_controller_t;
+
+typedef enum
+{
+    PID_LOOP_OUTER,
+    PID_LOOP_INNER
+} pid_loop_t;
+
+void pid_init();
+
+uint16_t pid_compute(pid_controller_t *pid, const float setpoint_angle, const float angle,
+                     const float rate);
+
+uint8_t pid_get_gains(pid_controller_t *pid, pid_loop_t loop, pid_gains_t *gains);
+
+void clamp(float *value, const float *max, const float *min);
+
+#endif // _PID_H_
